3a5afa77a1f5c16ef93541b5aa0bf2fdca82b8a9
ThinkerDreamer <74881094+ThinkerDreamer@users.noreply.github.com>
2022-07-23 19:37:03 -0400

    show/hide "please wait" message

--- randomSmileGenerator/app.js
+++ randomSmileGenerator/app.js
@@ -1,20 +1,26 @@
 smileBtn = document.querySelector("#smile-btn");
 imageDiv = document.querySelector('.image-div');
+waitDiv = document.querySelector("#wait-div");
 
 smileBtn.addEventListener("click", function(e) {
     e.preventDefault();
+    waitDiv.style.display = "block";
     if (imageDiv.firstChild) {
         imageDiv.removeChild(imageDiv.firstChild);
     }
     fetch(
-        "https://api.unsplash.com/photos/random?client_id=n9Daz6fSXsgIR1htycGsNjB0kmUjSvErWlyYVn1_mhs&query=cute%20happy")
-        .then(res => res.json())
-        .then(data => {
-            imageDiv.innerHTML = "<h2>Please wait...</h2>";
-            console.log(data);
-            const image = new Image();
-            image.src = data.urls.full;
-            imageDiv.appendChild(image);
-            // imageDiv.innerHTML = `<img src="${data.urls.full}" alt="${data.description}>`;
-        })
-    });
+      "https://api.unsplash.com/photos/random?client_id=n9Daz6fSXsgIR1htycGsNjB0kmUjSvErWlyYVn1_mhs&query=cute%20happy"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        const image = new Image();
+        image.src = data.urls.full;
+        imageDiv.appendChild(image);
+      })
+      .then(() =>
+        setTimeout(() => {
+          waitDiv.style.display = "none";
+        }, 500)
+      )
+});
